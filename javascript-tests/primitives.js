4b997e74d615df2149dc456c85d4c93665c36d89
Rylan Mendoza <rylan.mendoza@bellevuecollege.edu>
2019-10-30 10:41:19 -0700

    JavaRava

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,25 @@
+const myString = "Learning Javascript in class"
+const myFirstNumericVariable = 3;
+const myBoolean = true;
+const myNull = null;
+let myUndefined ;
+
+myUndefined = "Learning more Javascript in class";
+
+var oldStyle = "This is the way it was done before";
+let newChangeableStyle = "This allows you to declare a variable that can be changed."
+const constantVariable = "This variable can not be changed. Except...";
+const quotable = 'He said, "Hello". That\'s all folks';
+
+console.log(myString);
+console.log('Type:', typeof myString);
+console.log(myFirstNumericVariable);
+console.log('Type:', typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log('Type:', typeof myBoolean);
+console.log(myNull);
+console.log('Type:', typeof myNull);
+console.log(myUndefined);
+console.log('Type:', typeof myUndefined);
+console.log('Add 3 + 25 which equals', myFirstNumericVariable + 25);
+console.log('Add undefined:', myFirstNumericVariable + myUndefined);
