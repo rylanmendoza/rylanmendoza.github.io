e385461783dcebbb69d21d5f593c9e1cb0509d40
Rylan Mendoza <rylan.mendoza@bellevuecollege.edu>
2019-11-15 22:45:41 -0800

    I don't understand anything here

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -5,54 +5,37 @@ window.onload = () => {
 
     functionCallsAction.onclick = simpleFunction;
 
-    function simpleFunction(){
+    function simpleFunction() {
         console.log("Functions should consist of statements designed to perform a single task.");
         simpleFunctionDisplay.textContent = "simple";  
         functionCallsAction.onclick = 'simple'; 
+        }
+    };
+
+    function functionParameters(parameter) {
+        console.log(parameter);
     }
 
-function functionParameters(parameter) {
-    console.log(parameter);
-}
-
-function functionReturn() {
-    return ("Many functions return values.")
-}
-
-function add(a,b) {
-    const addResult = a + b;
-    console.log('Inside add', addResult);
-    return addResult;
-}
-
-simpleFunction();
-
-// adds a value to the the function that was empty
-functionParameters("Many functions take parameters.");
-
-functionReturn();
-
-const addResult = add(2,3);
-console.log('Add result:' , addResult);
-const returnValue = functionReturn(addResult);
-
-console.log(returnValue);
-
-const elfCode = {
-    appendToList: (list, value) => {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
+    function functionReturn() {
+        return ("Many functions return values.")
     }
-}
 
-const simpleFunction = document.getElementById('someAction');
-simpleFunction.onclick = function() {
-    console.log('Same')
-}
+    function add(a,b) {
+        const addResult = a + b;
+        console.log('Inside add', addResult);
+        return addResult;
+    }
 
-document.getElementById("addToListAction").value;
-document.getElementById("listDisplay").value;
-document.getElementById("nameDisplay").value;
+    simpleFunction();
 
+    // adds a value to the the function that was empty
+    functionParameters("Many functions take parameters.");
+
+    functionReturn();
+
+    const addResult = add(2,3);
+    console.log('Add result:' , addResult);
+    const returnValue = functionReturn(addResult);
+
+    console.log(returnValue);
 }
---
+++ javascript-tests/function-inputs.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+const feetToInches = (feet) => {
+    return this.feet * 12;
+        };
+    };
+const milesToFeet = (miles) => {
+    return this.miles * 5280;
+};
+const areaOfTriangle = (base,height) => {
+    return base * height / 2;
+};
+const areaOfCircle = (radius) => {
+    return area = Math.PI * radius ^ 2;
+};
