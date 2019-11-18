f71d5a1485751fb0f43e59865004f748a6a8b942
Rylan Mendoza <rylan.mendoza@bellevuecollege.edu>
2019-11-17 21:48:57 -0800

    hard projects

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-    // all code goes in here
     const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
     const functionCallsAction = document.getElementById('functionCallsAction');
 
@@ -12,30 +11,19 @@ window.onload = () => {
         }
     };
 
-    function functionParameters(parameter) {
-        console.log(parameter);
-    }
-
-    function functionReturn() {
-        return ("Many functions return values.")
-    }
-
-    function add(a,b) {
-        const addResult = a + b;
-        console.log('Inside add', addResult);
-        return addResult;
-    }
-
-    simpleFunction();
-
-    // adds a value to the the function that was empty
-    functionParameters("Many functions take parameters.");
-
-    functionReturn();
-
-    const addResult = add(2,3);
-    console.log('Add result:' , addResult);
-    const returnValue = functionReturn(addResult);
-
-    console.log(returnValue);
+function functionParameters(parameter) {
+    console.log(parameter);
 }
+
+function functionReturn() {
+    return ("Many functions return values.")
+}
+
+simpleFunction();
+
+// adds a value to the the function that was empty
+functionParameters("Many functions take parameters.");
+
+functionReturn();
+
+};
--- javascript-tests/function-inputs.js
+++ javascript-tests/function-inputs.js
@@ -1,14 +1,57 @@
 window.onload = () => {
+    const feetToInchesAction = 
+        document.getElementById('feetToInchesAction');
+
+    feetToInchesAction.onclick = () => {
+        const feetToInchesInput = document.getElementById('feetToInchesInput');
+        const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
+
+        feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
+    };
+};
 const feetToInches = (feet) => {
     return this.feet * 12;
-        };
-    };
+};
+
+const milesToFeetAction = 
+    document.getElementById('milesToFeetAction');
+
+feetToInchesAction.onclick = () => {
+    const milesToFeetInput = document.getElementById('milesToFeetInput');
+    const milesToFeetDisplay = document.getElementById('milesToFeetDisplay');
+
+    milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
+};
+
 const milesToFeet = (miles) => {
     return this.miles * 5280;
 };
+
+const areaOfTriangleAction = 
+    document.getElementById('areaOfTriangleAction');
+
+    areaOfTriangleAction.onclick = () => {
+    const areaOfTriangleInput = document.getElementById('areaOfTriangleInput');
+    const areaOfTriangleDisplay = document.getElementById('areaOfTriangleDisplay');
+
+    areaOfTriangleDisplay.textContent = areaOfTriangle(areaOfTriangleInput.value);
+};
+
 const areaOfTriangle = (base,height) => {
     return base * height / 2;
 };
+
+const areaOfCircleAction = 
+    document.getElementById('areaOfCircleAction');
+
+areaOfCircleAction.onclick = () => {
+    const areaOfCircleInput = document.getElementById('areaOfCircleInput');
+    const areaOfCircleDisplay = document.getElementById('areaOfCircleDisplay');
+
+    areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value);
+};
+
 const areaOfCircle = (radius) => {
     return area = Math.PI * radius ^ 2;
 };
+
