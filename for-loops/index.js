30115fcfd0c84c7fd48384f0245cf88eef4a51dd
Rylan Mendoza <rylan.mendoza@bellevuecollege.edu>
2019-12-06 22:05:37 -0800

    final changes

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,11 +1,12 @@
-window.onload = () => {
-    const elfCode = {
+const elfCode = {
         appendToList: (list, value) => {
             const li = document.createElement("li");
             li.appendChild(document.createTextNode(value));
             list.appendChild(li);
         }
     };
+    
+window.onload = () => {
     
     const listButton = document.getElementById('runForLoopAction');
 
